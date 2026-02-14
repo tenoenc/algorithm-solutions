4a9b0d49b83da85aed5a3d9da42095df638f0827
tenoenc <tenoenc@gmail.com>
2026-02-14 16:49:53 +0000

    Update from https://github.com/tenoenc/leetcode-solutions/commit/93cac45bad31ca94a4549954a30e3079875afab8

--- LeetCode/0626-exchange-seats/0626-exchange-seats.sql
+++ LeetCode/0626-exchange-seats/0626-exchange-seats.sql
@@ -1,9 +1,10 @@
 SELECT 
     (CASE
-        WHEN id % 2 = 1 AND id < (SELECT COUNT(*) FROM Seat) THEN id + 1
-        WHEN id % 2 = 1 AND id = (SELECT COUNT(*) FROM Seat) THEN id
+        WHEN id % 2 = 1 AND id < s2.cnt THEN id + 1
+        WHEN id % 2 = 1 AND id = s2.cnt THEN id
         ELSE id - 1
     END) AS id,
     student
-FROM Seat
+FROM Seat s1,
+    (SELECT COUNT(*) AS cnt FROM Seat) s2
 ORDER BY id ASC;
